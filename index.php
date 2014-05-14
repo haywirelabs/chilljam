132ed7786bf2b1eb19469241947e16de3ea2fdc7
Joe Osborne <josborne@haywirelabs.com>
2014-05-14 10:44:30 -0400

    list all files in directory recursive

--- index.php
+++ index.php
@@ -7,14 +7,22 @@
 
 <h1>Chilljam.com Repository</h1>
 
-
 <?php
 
-$path = realpath('/home/hedgehog001/chilltheuniverse.com/wp-content/uploads');
+$path = realpath('content');
 
 $objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path), RecursiveIteratorIterator::SELF_FIRST);
 foreach($objects as $name => $object){
-    echo $name + "</br>";
+  if(substr($name, -1) != "."){
+    $filePath = explode("/", $name);
+    unset($filePath[0]);
+    unset($filePath[1]);
+    unset($filePath[2]);
+      echo "$name "."</br>";
+      echo "<pre>";
+      var_dump($filePath);
+      echo "</pre>";
+  }
 }
 
 ?>
