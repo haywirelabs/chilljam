0c64c034548d55d988fdca25f82bb2ef64c953ba
Joe Osborne <josborne@haywirelabs.com>
2014-05-14 13:27:52 -0400

    test trueness

--- index.php
+++ index.php
@@ -14,37 +14,37 @@ $path = realpath('/home/hedgehog001/chilltheuniverse.com/wp-content/uploads');
 
 $objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path), RecursiveIteratorIterator::SELF_FIRST);
 
-$lastObject = unset(end($objects));
-
 var_dump($lastObject);
 
 foreach($objects as $name => $object){
-  $lastChar = substr($name, -1);
-  if($lastChar != "."){
-    $filePath = explode("/", $name);
-    $lastElement = end($filePath);
-    if(strpos($lastElement,".")){
-      unset($filePath[0]);
-      unset($filePath[1]);
-      unset($filePath[2]);
-      unset($filePath[3]);
-      $publish = end($filePath);
-      $publishMonth = prev($filePath);
-      $publishYear = prev($filePath);
-      $urlPath = 'http://chilljam.com/' . implode('/', $filePath);
-      $urlName = end($filePath);
-      echo '<p>';
-      echo '<a href="' . $urlPath . '" target="blank">' . $urlName . '</a>';
-      echo '<span class="byline">';
-      if (is_numeric($publishMonth)) {
-        $publishMonthName = date("F", mktime(0,0,0, $publishMonth, 10));
-        echo ' | Published in <strong>' . $publishMonthName . ' of ' . $publishYear . '</strong></br>';
+  if(true == true){
+    $lastChar = substr($name, -1);
+    if($lastChar != "."){
+      $filePath = explode("/", $name);
+      $lastElement = end($filePath);
+      if(strpos($lastElement,".")){
+        unset($filePath[0]);
+        unset($filePath[1]);
+        unset($filePath[2]);
+        unset($filePath[3]);
+        $publish = end($filePath);
+        $publishMonth = prev($filePath);
+        $publishYear = prev($filePath);
+        $urlPath = 'http://chilljam.com/' . implode('/', $filePath);
+        $urlName = end($filePath);
+        echo '<p>';
+        echo '<a href="' . $urlPath . '" target="blank">' . $urlName . '</a>';
+        echo '<span class="byline">';
+        if (is_numeric($publishMonth)) {
+          $publishMonthName = date("F", mktime(0,0,0, $publishMonth, 10));
+          echo ' | Published in <strong>' . $publishMonthName . ' of ' . $publishYear . '</strong></br>';
+        }
+        else{
+          echo ' | Published on <strong>' . $publishMonth . ' from ' . $publishYear . '</strong></br>';
+        }
+        echo '</span>';
+        echo '</p>';
       }
-      else{
-        echo ' | Published on <strong>' . $publishMonth . ' from ' . $publishYear . '</strong></br>';
-      }
-      echo '</span>';
-      echo '</p>';
     }
   }
 }
