dc30d20be5f81e937136128b437add3b4f115fa0
Joe Osborne <josborne@haywirelabs.com>
2014-05-14 11:22:19 -0400

    list media functionality complete

--- index.php
+++ index.php
@@ -13,15 +13,24 @@ $path = realpath('content');
 
 $objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path), RecursiveIteratorIterator::SELF_FIRST);
 foreach($objects as $name => $object){
-  if(substr($name, -1) != "."){
+  $lastChar = substr($name, -1);
+  if($lastChar != "."){
     $filePath = explode("/", $name);
-    unset($filePath[0]);
-    unset($filePath[1]);
-    unset($filePath[2]);
-      echo "$name "."</br>";
-      echo "<pre>";
-      var_dump($filePath);
-      echo "</pre>";
+    $lastElement = end($filePath);
+    if(strpos($lastElement,".")){
+      unset($filePath[0]);
+      unset($filePath[1]);
+      unset($filePath[2]);
+      $publish = end($filePath);
+      $publishMonth = prev($filePath);
+      $publishYear = prev($filePath);
+      $urlPath = 'http://localhost:8059/chilljam/' . implode('/', $filePath);
+      $urlName = end($filePath);
+      echo "<p>";
+      echo "Published: <strong>" . $publishMonth . "/" . $publishYear . "</strong></br>";
+      echo '<a href="' . $urlPath . '">' . $urlName . '</a></br>';
+      echo "</p>";
+    }
   }
 }
 
